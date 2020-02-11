0d9510823158cb293cfd015dbb9b31e218a8dfe8
Sirapop Para <worwee07@MacBook-Pro.local>
2020-02-11 16:23:54 +0700

    edited

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,19 +11,39 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
-//Write your code here.
+    int count,round=1;
+    do{
+        count = round;
+        for(int r=0;r<N;r++){
+            if(r==count) cout << "[" << d[r] << "]" << " ";
+            else cout << d[r] << " ";
+        }
+        cout << "=>" << " ";
+        for(int i = count;i>0;i--){
+            if(d[i]>d[i-1]){
+                swap(d,i,i-1);
+                count--;
+            }
+        }
+        for(int s=0;s<N;s++){
+            if(s==count) cout << "[" << d[s] << "]" << " ";
+            else cout << d[s] << " ";
+        }
+        cout << "\n";
+        round++;
+        if(round==N) break;
+    }while(1);
 
 }
 
 int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
-	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
-	cout << "\n\n";
-	
-	insertionSort(a,10);
-	
-	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+    int a[10] = {12,25,30,44,2,0,4,7,55,25};
+    cout << "Input Array:";
+    for(int i = 0; i < 10; i++) cout << a[i] << " ";
+    cout << "\n\n";
+
+    insertionSort(a,10);
+
+    cout << "\nSorted Array:";
+    for(int i = 0; i < 10; i++) cout << a[i] << " ";
 }
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -10,14 +10,42 @@ void findLocalMax(const double [][N], bool [][N]);
 void showMatrix(const bool [][N]);
 
 int main(){
-	double A[N][N]; 
-	bool B[N][N]; 
-	cout << "Input the matrix.....\n";
-	inputMatrix(A);
-	findLocalMax(A,B);
-	cout << "Result is \n";
-	showMatrix(B);
-	return 0;
+    double A[N][N];
+    bool B[N][N];
+    cout << "Input the matrix.....\n";
+    inputMatrix(A);
+    findLocalMax(A,B);
+    cout << "Result is \n";
+    showMatrix(B);
+    return 0;
 }
 
-// Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
+void inputMatrix(double A[][N]){
+    for(int i=0;i<N;i++){
+        cout << "Row " << i+1 << ": ";
+        for(int j=0;j<N;j++){
+            cin >> A[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double A[][N],bool B[][N]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(i==0||j==0||i==4||j==4) {B[i][j]= false; continue;}
+            if(A[i][j]>=A[i-1][j]&&A[i][j]>=A[i][j-1]&&A[i][j]>=A[i][j+1]&&A[i][j]>=A[i+1][j])  B[i][j]=true;
+            else B[i][j]= false;
+        }
+    }
+}
+
+void showMatrix(const bool B[][N]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(B[i][j]==1) cout << "1";
+            else cout << "0";
+            cout << " ";
+        }
+        cout << "\n";
+    }
+}
